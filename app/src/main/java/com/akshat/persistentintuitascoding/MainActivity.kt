d4151db12585881ed8e616a12174321f86e7380a
Akshat <akshat@Akshats-MacBook-Pro.local>
2025-08-12 10:17:03 -0400

    Second task - There should be a timer in each cell which should increment every second when the listItem is visible.

--- app/src/main/java/com/akshat/persistentintuitascoding/MainActivity.kt
+++ app/src/main/java/com/akshat/persistentintuitascoding/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.akshat.persistentintuitascoding.ui.theme.PersistentIntuitASCodingTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +43,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TimeList(modifier: Modifier = Modifier) {
-    val items = List(30) { "Item +$it" }
-    LazyColumn {
-        items(items.size) {
-            Text(
-                text = "$it"
-            )
+    val listSize = 30
+    val timers = remember { MutableList(listSize) { mutableStateOf(0) } }
+    val listState = rememberLazyListState()
+
+    // Detect visible items and update timers
+    LaunchedEffect(listState) {
+        while (isActive) {
+            val visibleIndexes = listState.layoutInfo.visibleItemsInfo.map { it.index }
+            visibleIndexes.forEach { index ->
+                timers[index].value++
+            }
+            delay(1000)
         }
     }
+
+    LazyColumn(state = listState) {
+        itemsIndexed(timers) { index, timerState ->
+            TimerListItem(index, timerState.value)
+        }
+    }
+}
+
+@Composable
+fun TimerListItem(index: Int, timer: Int) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Item $index")
+        Text(text = "$timer s")
+    }
+
+
 }
 
 @Preview(showBackground = true)
